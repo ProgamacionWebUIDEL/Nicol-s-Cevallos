f940522b5c9282e22c57adbd5c48b057a1a653fb
nicevall <uide.edu.ec@uide.edu.ec>
2024-07-23 12:27:06 -0500

    3

---
+++ src/app/components.tsx
@@ -0,0 +1,17 @@
+
+function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
+    );
+  }
+
+export default function components(){
+    return (
+        <section>
+            <Profile/>
+        </section>
+    );
+}
